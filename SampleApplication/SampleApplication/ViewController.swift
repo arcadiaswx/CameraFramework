1f6d2270a807aeb4fcc07c81b6f62c292c77ee08
Craig Booker <info@arcadiasoftworks.com>
2019-02-07 10:50:30 -0600

    Added base project files to create Framework and sample application.

---
+++ SampleApplication/SampleApplication/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SampleApplication
+//
+//  Created by Craig Booker on 2/7/19.
+//  Copyright © 2019 Arcadia Softworks. All rights reserved.
+//
+
+import UIKit
+import CameraFramework
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
